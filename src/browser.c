5841baf2d357237acd59384d607af55409ea17bb
hasufell <hasufell@posteo.de>
2014-02-10 02:29:59 +0100

    GUI: rewrite main window/menu/toolbar for newer APIs
    Use GtkUIManager and GtkActionGroup. Still left for gtk3
    is converting gtk_vbox_new to gtk_box_new.

--- src/browser.c
+++ src/browser.c
@@ -38,30 +38,76 @@
 
 Browser *browser = NULL;
 
-static void browser_remove_old_thumbs (void);
-static void browser_clear_cache (void);
-static void browser_remove_old_comments (void);
-static void browser_clear_comments (void);
-
 #define browser_about  about_dialog
 
-static GtkItemFactoryEntry browser_menu_factory[] = {
-    {N_("/_File"), NULL, NULL, 0, "<Branch>"},
-    {N_("/File/E_xit"), "<control>X", browser_destroy, 0, NULL},
-    {N_("/_Tools"), NULL, NULL, 0, "<Branch>"},
-    {N_("/Tools/Delete Old Thumbnails"), NULL, browser_remove_old_thumbs, 0,
-     NULL},
-    {N_("/Tools/Delete All Thumbnails"), NULL, browser_clear_cache, 0, NULL},
-    {N_("/Tools/---"), NULL, NULL, 0, "<Separator>"},
-    {N_("/Tools/Delete Old Comments"), NULL, browser_remove_old_comments, 0,
-     NULL},
-    {N_("/Tools/Delete All Comments"), NULL, browser_clear_comments, 0, NULL},
-    {N_("/_Help"), NULL, NULL, 0, "<LastBranch>"},
-    {N_("/Help/A_bout"), "<control>B", browser_about, 0, NULL}
+static const gchar *browser_ui_xml =
+"<ui>"
+	"<menubar name=\"MainMenu\">"
+		"<menu name=\"FileMenu\" action=\"FileMenuAction\">"
+			"<menuitem name=\"Preferences\" action=\"PrefsAction\" />"
+			"<menuitem name=\"Quit\" action=\"QuitAction\" />"
+			"<placeholder name=\"FileMenuAdditions\" />"
+		"</menu>"
+		"<menu name=\"ToolsMenu\""
+			"action=\"ToolsMenuAction\">"
+			"<menuitem name=\"DeleteOldThumb\""
+					"action=\"DeleteOldThumbAction\"/>"
+			"<menuitem name=\"DeleteAllThumb\""
+					"action=\"DeleteAllThumbAction\"/>"
+			"<menuitem name=\"DeleteOldComments\""
+					"action=\"DeleteOldCommentsAction\"/>"
+			"<menuitem name=\"DeleteAllComments\""
+					"action=\"DeleteAllCommentsAction\"/>"
+	    "</menu>"
+		"<menu name=\"HelpMenu\""
+				"action=\"HelpMenuAction\">"
+			"<menuitem name=\"About\" action=\"AboutAction\" />"
+		"</menu>"
+	"</menubar>"
+"</ui>";
+
+static GtkActionEntry browser_ui_entries[] = {
+	/*
+	 * menu
+	 */
+
+	/* file menu */
+	{ "FileMenuAction", NULL, N_("_File"),	/* name, stock id, label */
+		NULL, NULL,							/* accelerator, tooltip */
+		NULL },					 			/* callback */
+	{ "PrefsAction", GTK_STOCK_PROPERTIES, N_("_Preferences"),
+		"<control>P", "Preferences",
+		G_CALLBACK(browser_prefs) },
+	{ "QuitAction", GTK_STOCK_QUIT, N_("_Quit"),
+		"<control>Q", "Quit",
+		G_CALLBACK(browser_destroy) },
+	/* help menu */
+	{ "HelpMenuAction", GTK_STOCK_HELP, N_("_Help"),
+		NULL, NULL,
+		NULL },
+	{ "AboutAction", GTK_STOCK_ABOUT, N_("_About"),
+		"<control>B", "About",
+		G_CALLBACK(browser_about) },
+	/* tools menu */
+	{ "ToolsMenuAction", NULL, N_("_Tools"),
+		NULL, NULL,
+		NULL },
+	{ "DeleteOldThumbAction", NULL, N_("_Delete Old Thumbnails"),
+		NULL, NULL,
+		G_CALLBACK(browser_remove_old_thumbs) },
+	{ "DeleteAllThumbAction", NULL, N_("Delete All Thumbnails"),
+		NULL, NULL,
+		G_CALLBACK(browser_clear_cache) },
+	{ "DeleteOldCommentsAction", NULL, N_("Delete Old Comments"),
+		NULL, NULL,
+		G_CALLBACK(browser_remove_old_comments) },
+	{ "DeleteAllCommentsAction", NULL, N_("Delete All Comments"),
+		NULL, NULL,
+		G_CALLBACK(browser_clear_comments) }
+
 };
 
-static int browser_menu_factory_count =
-    sizeof (browser_menu_factory) / sizeof (GtkItemFactoryEntry);
+static guint n_browser_ui_entries = G_N_ELEMENTS(browser_ui_entries);
 
 static gint timer_id;
 
@@ -71,43 +117,35 @@ static gint timer_id;
  *-------------------------------------------------------------------
  */
 
-static int
-cb_browser_select_dir (gpointer data)
+static void
+browser_remove_old_thumbs(void)
 {
-    browser_select_dir ((gchar *) data);
-
-    return FALSE;
+    cache_maintain_home(CACHE_THUMBS, FALSE);
 }
 
 static void
-browser_remove_old_thumbs (void)
+browser_clear_cache(void)
 {
-    cache_maintain_home (CACHE_THUMBS, FALSE);
+    cache_maintain_home(CACHE_THUMBS, TRUE);
+	thumbview_refresh();
 }
 
 static void
-browser_clear_cache (void)
+browser_remove_old_comments(void)
 {
-    cache_maintain_home (CACHE_THUMBS, TRUE);
-    thumbview_refresh ();
+    cache_maintain_home(CACHE_COMMENTS, FALSE);
 }
 
 static void
-browser_remove_old_comments (void)
+browser_clear_comments(void)
 {
-    cache_maintain_home (CACHE_COMMENTS, FALSE);
+    cache_maintain_home(CACHE_COMMENTS, TRUE);
 }
 
 static void
-browser_clear_comments (void)
+browser_prefs(void)
 {
-    cache_maintain_home (CACHE_COMMENTS, TRUE);
-}
-
-static void
-browser_prefs ()
-{
-    prefs_win_open_idle ("/General");
+    prefs_win_open_idle("/General");
 }
 
 /*
@@ -115,195 +153,203 @@ browser_prefs ()
  * public functions
  *-------------------------------------------------------------------
  */
-
 void
-browser_create (gchar* path)
+browser_create(const gchar *start_path)
 {
     GtkWidget *vbox;
-    GdkPixmap *icon_pix;
-    GdkBitmap *icon_mask;
-    GtkAccelGroup *accel;
+    GdkPixbuf *icon_pix;
     GtkWidget *menu;
-    GtkWidget *toolbar;
     GtkWidget *hpaned;
     GtkWidget *vpaned;
-    GtkWidget *iconw;
     GtkWidget *hbox;
     GtkWidget *label;
+	GError *error;
+	guint ui_id;
 
-    browser = g_new (Browser, 1);
+	GtkActionGroup *action_group;
+
+    browser = g_new(Browser, 1);
 
 	/* check path argument, if any */
-	if (isdir(path)) {
-		browser->current_path = g_string_new(path);
+	if (isdir(start_path)) {
+		browser->current_path = g_string_new(start_path);
 	} else {
-		if (path)
-			fprintf(stderr, "\"%s\" is not a valid dir!\n", path);
-	    browser->current_path = g_string_new (conf.startup_dir);
+		if (start_path)
+			fprintf(stderr, "\"%s\" is not a valid dir!\n", start_path);
+	    browser->current_path = g_string_new(conf.startup_dir);
 	}
 
-    browser->last_path = g_string_new ("");
-    browser->filelist = (FileList *) file_list_init ();
+    browser->last_path = g_string_new("");
+    browser->filelist = (FileList *)file_list_init();
 
-    browser->window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title (GTK_WINDOW (browser->window), "PornView");
-    gtk_widget_realize (browser->window);
+    browser->window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW (browser->window), "PornView");
+    gtk_widget_realize(browser->window);
 
-    icon_pix =
-	gdk_pixmap_create_from_xpm_d (G_WINDOW (browser->window), &icon_mask,
-				      NULL, pornview_xpm);
-    gdk_window_set_icon (G_WINDOW (browser->window), NULL, icon_pix,
-			 icon_mask);
+    icon_pix = gdk_pixbuf_new_from_xpm_data(pornview_xpm);
 
-    gtk_signal_connect (GTK_OBJECT (browser->window), "delete_event",
-			GTK_SIGNAL_FUNC (browser_destroy), NULL);
+    gtk_window_set_icon(GTK_WINDOW(browser->window), icon_pix);
 
-    gtk_window_set_default_size (GTK_WINDOW (browser->window),
+    g_signal_connect(G_OBJECT(browser->window), "delete_event",
+			G_CALLBACK(browser_destroy), NULL);
+
+    gtk_window_set_default_size(GTK_WINDOW(browser->window),
 				 conf.window_width, conf.window_height);
 
-    vbox = gtk_vbox_new (FALSE, 0);
-    gtk_widget_show (vbox);
-    gtk_container_add (GTK_CONTAINER (browser->window), vbox);
+	/* TODO: use gtk_box_new for gtk+-3 */
+    vbox = gtk_vbox_new(FALSE, 0);
+    gtk_widget_show(vbox);
+    gtk_container_add(GTK_CONTAINER(browser->window), vbox);
+
 
     /*
      * main menu
      */
-    accel = gtk_accel_group_new ();
-    browser->menu =
-	gtk_item_factory_new (GTK_TYPE_MENU_BAR, "<browser_menu>", accel);
+	browser->menu_toolbar = gtk_ui_manager_new();
 
-#ifdef ENABLE_NLS
-    gtk_item_factory_set_translate_func (browser->menu,
-					 (GtkTranslateFunc) menu_translate,
-					 NULL, NULL);
-#endif
+	action_group = gtk_action_group_new("BrowserActions");
+	gtk_action_group_add_actions(action_group, browser_ui_entries, n_browser_ui_entries, NULL);
+	gtk_ui_manager_insert_action_group(browser->menu_toolbar, action_group, 0);
 
-    gtk_item_factory_create_items (browser->menu, browser_menu_factory_count,
-				   browser_menu_factory, NULL);
-    menu = gtk_item_factory_get_widget (browser->menu, "<browser_menu>");
+	error = NULL;
+	ui_id = gtk_ui_manager_add_ui_from_string(browser->menu_toolbar,
+			browser_ui_xml,
+			-1,
+			&error);
+	if (error) {
+		g_message("building menus failed: %s", error->message);
+		g_error_free(error);
+	}
 
-    gtk_box_pack_start (GTK_BOX (vbox), menu, FALSE, FALSE, 0);
-    gtk_widget_show (menu);
 
-    /*
-     * toolbar
-     */
-    toolbar = gtk_toolbar_new ();
+	/*
+	 * menubar/toolbar
+	 */
+	/* TODO: use gtk_ui_manager_remove_ui() later */
+	menu = gtk_ui_manager_get_widget(browser->menu_toolbar, "/MainMenu");
 
-    gtk_box_pack_start (GTK_BOX (vbox), toolbar, FALSE, TRUE, 0);
-    gtk_widget_show (toolbar);
+	gtk_box_pack_start(GTK_BOX(vbox), menu, FALSE, FALSE, 0);
 
-    iconw = pixbuf_create_pixmap_from_xpm_data (exit_xpm);
-    gtk_toolbar_append_item (GTK_TOOLBAR (toolbar), NULL, _("Exit"), NULL,
-			     iconw, (GtkSignalFunc) browser_destroy, NULL);
+	gtk_window_add_accel_group(GTK_WINDOW(browser->window),
+			gtk_ui_manager_get_accel_group(browser->menu_toolbar));
 
-    gtk_toolbar_append_space (GTK_TOOLBAR (toolbar));
+	gtk_widget_show (menu);
 
-    iconw = pixbuf_create_pixmap_from_xpm_data (options_xpm);
-    gtk_toolbar_append_item (GTK_TOOLBAR (toolbar), NULL, _("Preference"),
-			     NULL, iconw, (GtkSignalFunc) browser_prefs,
-			     NULL);
 
-    hpaned = gedo_hpaned_new ();
-    gtk_container_add (GTK_CONTAINER (vbox), hpaned);
-    gtk_widget_show (hpaned);
+	/*
+	 * prepare boxes for dirtree, thumbview etc
+	 */
+	hpaned = gedo_hpaned_new ();
+	gtk_container_add (GTK_CONTAINER (vbox), hpaned);
+	gtk_widget_show (hpaned);
 
-    vpaned = gedo_vpaned_new ();
-    gtk_widget_show (vpaned);
-    gedo_paned_add1 (GEDO_PANED (hpaned), vpaned);
+	vpaned = gedo_vpaned_new ();
+	gedo_paned_add1 (GEDO_PANED (hpaned), vpaned);
+	gtk_widget_show (vpaned);
 
-    /*
-     * dirtree
-     */
-    dirview_create (browser->current_path->str, browser->window);
 
-    gedo_paned_add1 (GEDO_PANED (vpaned), DIRVIEW_CONTAINER);
+	/*
+	 * dirtree
+	 */
+	dirview_create (browser->current_path->str, browser->window);
+	gedo_paned_add1 (GEDO_PANED (vpaned), DIRVIEW_CONTAINER);
+	commentview = comment_view_create ();
+	browser->notebook = commentview->notebook;
 
-    commentview = comment_view_create ();
+	gtk_widget_set_size_request(dirview->scroll_win,
+			conf.dirtree_width, conf.dirtree_height);
 
-    browser->notebook = commentview->notebook;
 
     /*
      * videoplay
      */
-#ifdef ENABLE_MOVIE
-    videoplay_create ();
-    label = gtk_label_new (_(" Preview "));
-    gtk_notebook_prepend_page (GTK_NOTEBOOK (browser->notebook),
-			       VIDEOPLAY_CONTAINER, label);
+/* #ifdef ENABLE_MOVIE */
+    /* videoplay_create (); */
+    /* label = gtk_label_new (_(" Preview ")); */
+    /* gtk_notebook_prepend_page (GTK_NOTEBOOK (browser->notebook), */
+				   /* VIDEOPLAY_CONTAINER, label); */
 
-    gtk_notebook_set_page (GTK_NOTEBOOK (browser->notebook), 0);
+    /* gtk_notebook_set_current_page(GTK_NOTEBOOK (browser->notebook), 0); */
 
-#endif
+/* #endif */
+
 
     /*
      * imageview
      */
-    imageview_create ();
-    label = gtk_label_new (_(" Preview "));
-    gtk_notebook_prepend_page (GTK_NOTEBOOK (browser->notebook),
-			       IMAGEVIEW_CONTAINER, label);
+	imageview_create ();
+	label = gtk_label_new (_(" Preview "));
+	gtk_notebook_prepend_page (GTK_NOTEBOOK (browser->notebook),
+				   IMAGEVIEW_CONTAINER, label);
 
 #ifndef ENABLE_MOVIE
-    gtk_notebook_set_page (GTK_NOTEBOOK (browser->notebook), 0);
+	gtk_notebook_set_current_page(GTK_NOTEBOOK (browser->notebook), 0);
 #endif
 
-    gedo_paned_add2 (GEDO_PANED (hpaned), commentview->main_vbox);
+	gedo_paned_add2 (GEDO_PANED (hpaned), commentview->main_vbox);
+
 
     /*
      * thumbview
      */
-    thumbview_create (browser->window);
-    gedo_paned_add2 (GEDO_PANED (vpaned), THUMBVIEW_CONTAINER);
+	thumbview_create (browser->window);
+	gedo_paned_add2 (GEDO_PANED (vpaned), THUMBVIEW_CONTAINER);
+
+	/* this sets focus on the _dirview_ */
+	gtk_widget_set_can_focus(THUMBVIEW_CONTAINER, TRUE);
+	gtk_widget_grab_focus(THUMBVIEW_CONTAINER);
+
 
     /*
      * statusbar
      */
-    hbox = gtk_hbox_new (FALSE, 0);
-    gtk_widget_set_name (hbox, "StatusBarContainer");
-    gtk_box_pack_end (GTK_BOX (vbox), hbox, FALSE, FALSE, 0);
-    gtk_widget_show (hbox);
+	/* TODO: use gtk_box_new for gtk+-3 */
+	hbox = gtk_hbox_new (FALSE, 0);
+	gtk_widget_set_name (hbox, "StatusBarContainer");
+	gtk_box_pack_end (GTK_BOX (vbox), hbox, FALSE, FALSE, 0);
+	gtk_widget_show (hbox);
 
-    browser->status_dir = gtk_statusbar_new ();
-    gtk_widget_set_name (browser->status_dir, "StatusBar1");
-    gtk_container_border_width (GTK_CONTAINER (browser->status_dir), 1);
-    gtk_widget_set_usize (browser->status_dir, 80, 15);
-    gtk_box_pack_start (GTK_BOX (hbox), browser->status_dir, FALSE, FALSE, 0);
-    gtk_widget_show (browser->status_dir);
+	browser->status_dir = gtk_statusbar_new ();
+	gtk_widget_set_name (browser->status_dir, "StatusBar1");
+	gtk_container_set_border_width (GTK_CONTAINER (browser->status_dir), 1);
+	gtk_widget_set_size_request (browser->status_dir, 80, 15);
+	gtk_box_pack_start (GTK_BOX (hbox), browser->status_dir, FALSE, FALSE, 0);
+	gtk_widget_show (browser->status_dir);
 
-    browser->status_name = gtk_statusbar_new ();
-    gtk_widget_set_name (browser->status_name, "StatusBar2");
-    gtk_container_border_width (GTK_CONTAINER (browser->status_name), 1);
-    gtk_widget_set_usize (browser->status_name, 150, -1);
-    gtk_box_pack_start (GTK_BOX (hbox), browser->status_name, TRUE, TRUE, 0);
-    gtk_widget_show (browser->status_name);
+	browser->status_name = gtk_statusbar_new ();
+	gtk_widget_set_name (browser->status_name, "StatusBar2");
+	gtk_container_set_border_width (GTK_CONTAINER (browser->status_name), 1);
+	gtk_widget_set_size_request (browser->status_name, 150, -1);
+	gtk_box_pack_start (GTK_BOX (hbox), browser->status_name, TRUE, TRUE, 0);
+	gtk_widget_show (browser->status_name);
 
-    browser->status_image = gtk_statusbar_new ();
-    gtk_widget_set_name (browser->status_image, "StatusBar3");
-    gtk_container_border_width (GTK_CONTAINER (browser->status_image), 1);
-    gtk_widget_set_usize (browser->status_image, 150, -1);
-    gtk_box_pack_start (GTK_BOX (hbox), browser->status_image, TRUE, TRUE, 0);
-    gtk_widget_show (browser->status_image);
+	browser->status_image = gtk_statusbar_new ();
+	gtk_widget_set_name (browser->status_image, "StatusBar3");
+	gtk_container_set_border_width (GTK_CONTAINER (browser->status_image), 1);
+	gtk_widget_set_size_request (browser->status_image, 150, -1);
+	gtk_box_pack_start (GTK_BOX (hbox), browser->status_image, TRUE, TRUE, 0);
+	gtk_widget_show (browser->status_image);
 
-    browser->progress = gtk_progress_bar_new ();
-    gtk_widget_set_name (browser->progress, "ProgressBar");
-    gtk_box_pack_end (GTK_BOX (hbox), browser->progress, FALSE, FALSE, 0);
-    gtk_widget_show (browser->progress);
+	browser->progress = gtk_progress_bar_new ();
+	gtk_widget_set_name (browser->progress, "ProgressBar");
+	gtk_box_pack_end (GTK_BOX (hbox), browser->progress, FALSE, FALSE, 0);
+	gtk_widget_show (browser->progress);
 
-    gtk_widget_show (browser->window);
+	gtk_widget_show (browser->window);
 
-#ifdef ENABLE_MOVIE
-    gtk_widget_hide (gtk_notebook_get_nth_page
-		     (GTK_NOTEBOOK (browser->notebook), 1));
-    gtk_notebook_set_page (GTK_NOTEBOOK (browser->notebook), 0);
-#endif
+/* #ifdef ENABLE_MOVIE */
+    /* gtk_widget_hide (gtk_notebook_get_nth_page */
+			 /* (GTK_NOTEBOOK (browser->notebook), 1)); */
+    /* gtk_notebook_set_current_page(GTK_NOTEBOOK (browser->notebook), 0); */
+/* #endif */
 
-    dirview_scroll_center ();
-    timer_id = gtk_timeout_add (100, cb_browser_select_dir, browser->current_path->str);
+	dirview_scroll_center ();
+
+	browser_select_dir(browser->current_path->str);
 }
 
 void
-browser_destroy (void)
+browser_destroy(void)
 {
     file_list_free (BROWSER_FILE_LIST);
 
@@ -333,8 +379,8 @@ browser_destroy (void)
     videoplay_destroy ();
 #endif
 
-    imageview_destroy ();
-    thumbview_destroy ();
+	imageview_destroy ();
+	thumbview_destroy ();
     dirview_destroy ();
 
     g_free (browser);
@@ -342,38 +388,50 @@ browser_destroy (void)
     gtk_main_quit ();
 }
 
+/* FIXME: ignores uppercase .JPEG etc */
 void
-browser_select_dir (gchar * path)
+browser_select_dir(gchar *path)
 {
-    gchar  *title;
-    char    buf[PATH_MAX];
+    gchar *title,
+		  *old_path = g_get_current_dir();
+    char buf[PATH_MAX];
 
-    if (!strcmp (browser->last_path->str, path))
-	return;
+	/* gdk_window_freeze_updates(GDK_WINDOW(browser->window)); */
+
+    if (!path || !strcmp(browser->last_path->str, path))
+		return;
 
 #ifdef ENABLE_MOVIE
-    videoplay_clear ();
+    videoplay_clear();
 #endif
-    comment_view_clear (commentview);
-    thumbview_stop ();
-    imageview_stop ();
+	comment_view_clear(commentview);
+	thumbview_stop();
+	imageview_stop ();
 
-    viewtype_set (VIEWTYPE_IMAGEVIEW);
+    viewtype_set(VIEWTYPE_IMAGEVIEW);
 
-    g_string_assign (browser->last_path, path);
-    g_string_assign (browser->current_path, path);
+    g_string_assign(browser->last_path, path);
+    g_string_assign(browser->current_path, path);
 
-    thumbview_clear ();
+	thumbview_clear();
 
-    file_list_create (browser->filelist, path);
+	/* file_list_create relies on relative paths,
+	 * chdir even if it is redundant */
+	if (chdir(path)) /* chdir failed */
+		return;
+    file_list_create(browser->filelist, path);
+	chdir(old_path);
+	g_free(old_path);
 
-    title = g_strconcat ("PornView - ", g_basename (path), NULL);
-    gtk_window_set_title (GTK_WINDOW (browser->window), title);
-    g_free (title);
+    title = g_strconcat("PornView - ", g_path_get_basename(path), NULL);
+    gtk_window_set_title(GTK_WINDOW(browser->window), title);
+    g_free(title);
 
-    snprintf (buf, PATH_MAX, "  %d", browser->filelist->num);
-    gtk_statusbar_pop (GTK_STATUSBAR (BROWSER_STATUS_DIR), 1);
-    gtk_statusbar_push (GTK_STATUSBAR (BROWSER_STATUS_DIR), 1, buf);
+    snprintf(buf, PATH_MAX, "  %d", browser->filelist->num);
+    gtk_statusbar_pop (GTK_STATUSBAR(BROWSER_STATUS_DIR), 1);
+    gtk_statusbar_push (GTK_STATUSBAR(BROWSER_STATUS_DIR), 1, buf);
 
-    thumbview_add (browser->filelist);
+	thumbview_add(browser->filelist);
+
+	/* gtk_widget_thaw_child_notify(GTK_WIDGET(dirview->dirtree)); */
 }
--- src/browser.h
+++ src/browser.h
@@ -10,28 +10,9 @@
 #ifndef __BROWSER_H__
 #define __BROWSER_H__
 
+#include <gtk/gtk.h>
+
 #include "file_list.h"
-
-typedef struct _Browser Browser;
-
-struct _Browser
-{
-    GtkWidget      *window;
-    GtkItemFactory *menu;
-    GtkWidget      *notebook;
-
-    GtkWidget      *status_dir;
-    GtkWidget      *status_name;
-    GtkWidget      *status_image;
-    GtkWidget      *progress;
-
-    GString        *current_path;
-    GString        *last_path;
-
-    FileList       *filelist;
-};
-
-extern Browser *browser;
 
 #define BROWSER               browser
 #define BROWSER_WINDOW        browser->window
@@ -44,8 +25,80 @@ extern Browser *browser;
 #define BROWSER_LAST_PATH     browser->last_path->str
 #define BROWSER_FILE_LIST     browser->filelist
 
-void            browser_create (gchar* path);
-void            browser_destroy (void);
-void            browser_select_dir (gchar * path);
+typedef struct _Browser Browser;
+
+/**
+ * Describes the browser and commonly
+ * used properties.
+ */
+struct _Browser {
+	/**
+	 * Toplevel window.
+	 */
+    GtkWidget *window;
+	/**
+	 * Menu + Toolbar.
+	 */
+    GtkUIManager *menu_toolbar;
+    GtkWidget *notebook;
+
+    GtkWidget *status_dir;
+    GtkWidget *status_name;
+    GtkWidget *status_image;
+    GtkWidget *progress;
+
+    GString *current_path;
+    GString *last_path;
+	/**
+	 * All image files in the current directory.
+	 */
+    FileList *filelist;
+};
+
+extern Browser *browser;
+
+/**
+ * Remove old cached thumbnails.
+ */
+static void browser_remove_old_thumbs(void);
+
+/**
+ * Remove all cached thumbnails.
+ */
+static void browser_clear_cache(void);
+
+/**
+ * Remove old cached comments.
+ */
+static void browser_remove_old_comments(void);
+
+/**
+ * Remove all cached comments
+ */
+static void browser_clear_comments(void);
+
+/**
+ * Open the preferences window.
+ */
+static void browser_prefs(void);
+
+/**
+ * Create a new browser window.
+ *
+ * @param start_path initial path for the dirview
+ */
+void browser_create(const gchar* start_path);
+
+/**
+ * Destroy the browser and clear all widgets/allocs/refs.
+ */
+void browser_destroy(void);
+
+/**
+ * Select a directory, update thumbnails, clear imageview
+ * and videoplay.
+ */
+void browser_select_dir(gchar *path);
+
 
 #endif /* __BROWSER_H__ */
