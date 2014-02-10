0ac445ad375a9f71eb2d9fb09bc8aeeeb426d399
hasufell <hasufell@posteo.de>
2014-02-10 02:11:01 +0100

    BUILD: non-interactive gettextize

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 wget 'http://savannah.gnu.org/cgi-bin/viewcvs/*checkout*/config/config/config.sub'
 wget 'http://savannah.gnu.org/cgi-bin/viewcvs/*checkout*/config/config/config.guess'
-gettextize --po-dir=po && mv po/Makevars.template po/Makevars && aclocal -I m4 && libtoolize --copy --force --install --automake && autoconf && autoheader && automake --add-missing --copy
+
+cp -f $(type -p gettextize) .
+sed -i -e 's:read dummy < /dev/tty::' gettextize
+
+./gettextize -f --no-changelog --po-dir=po && mv po/Makevars.template po/Makevars && aclocal -I m4 && libtoolize --copy --force --install --automake && autoconf && autoheader && automake --add-missing --copy
